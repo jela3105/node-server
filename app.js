0d1796e0cdf8681bf3bd14919249302ca3f6ca6a
jela3105 <jela3105@gmail.com>
2021-04-05 16:21:34 -0500

    Set up server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("Hello world!");
+  res.end();
+});
+
+server.listen(8080);
+
+console.log("Listening in the port 8080");
