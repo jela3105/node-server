aa9c29d2b397f63ed4f28ea9fe2d81ab1d5eff25
jela3105 <jela3105@gmail.com>
2021-04-05 20:39:27 -0500

    Return a json on request get

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Hello world!");
+  res.writeHead(200, { "Content-Type": "application/json" });
+  const person = {
+    id: 1,
+    name: "JELA",
+  };
+  res.write(JSON.stringify(person));
   res.end();
 });
 
