ec82607953f71a071b5f1c9a155819982e10a459
jela3105 <jela3105@gmail.com>
2021-04-08 19:20:48 -0500

    Implement hbs in all pages

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express");
+const hbs = require("hbs");
+
 const app = express();
 
+//handlebars
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("home", { name: "Jela3105", title: "Web server with node" });
 });
+
 app.get("/hello-world", (req, res) => {
   res.send("hello world");
 });
+
 app.get("/generic", (req, res) => {
-  res.sendFile(__dirname + "/public/generic.html");
+  res.render("generic", { name: "Jela3105", title: "Web server with node" });
 });
+
 app.get("/elements", (req, res) => {
-  res.sendFile(__dirname + "/public/elements.html");
+  res.render("elements", { name: "Jela3105", title: "Web server with node" });
 });
+
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/public/404.html");
 });
+
 app.listen(8080);
